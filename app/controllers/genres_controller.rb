6540ddebb5eb8d500953b43fb1dfc238f817c98a
Derek Townsend <derekjavon2@gmail.com>
2019-08-12 16:59:54 -0500

    Show Page for Genre and actors done

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,24 @@
 class GenresController < ApplicationController
+  def show
+    genre = Genre.find_by(id: params[:id])
+    page = params["page"]
+    page = page.to_i
+    ending_page = page*24
+    starting_page = ending_page-24
+    movies = genre.movies
+    movies_max = movies.count
+    
+    if movies
+      movies = movies[starting_page...ending_page]
+      p movies
+      movies = JSON.parse(MovieSerializer.new(movies.to_a, movies_max).to_serialized_json)
+      render json:  {genre: genre, movies: movies}
+    else
+      render json: {movies: []}
+    end
+    # total: Movie.all.count
+
+    # movies = JSON.parse(MovieSerializer.new(movies.to_a, movies_max).to_serialized_json)["movies"]
+    # p movies["movies"]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,51 @@ gem 'rest-client'
 # Rating.destroy_all
 
 # rating_amounts = (1...100).to_a
+ comments = [
+   "This Movie is pretty good. It was heart warming thought provoking and really made me think deeply about the human condition and what it means to be human. My eyes have never been this opened by a peice of media. I feel truely awakened.",
 
+  "Pirates are evil? The Marines are righteous? These terms have always changed throughout the course of history! Kids who have never seen peace and kids who have never seen war have different values! Those who stand at the top determine what's wrong and what's right! This very place is neutral ground! Justice will prevail, you say? But of course it will! Whoever wins this war becomes justice!",
+
+  "Once, the Lord of Light banished Dark, and all that stemmed from humanity. And men assumed a fleeting form. These are the roots of our world. Men are props on the stage of life, and no matter how tender, how exquisite... A lie will remain a lie!",
+
+  "I may be but small, but I will die a colossus.",
+
+  "The dragons shall never be forgotten… We knights fought valiantly, but for every one of them, we lost three score of our own. Exhiliration, pride, hatred, rage… The dragons teased out our dearest emotions. ... Thou will understand, one day. At thy twilight, old thoughts return, in great waves of nostalgia",
+
+  "Brother, unyielding sword of Lothric's Prince. Rise, if you would... For that is our curse",
+
+  "I love heroes, but I don't want to be one. Do you even know what a hero is!? For example, you have some meat. Pirates will feast on the meat, but the hero will distribute it among the people! I want to eat the meat",
+
+  "Don't ever think there's any perfect society made by humans!! If you think that way you'll overlook the enemy!! Don't be fooled by appearances!!",
+
+  "If I can't even protect my captain's dream, then whatever ambition I have is nothing but talk! Luffy must be the man who becomes the Pirate King!",
+
+  "Compared to the 'righteous' greed of the rulers, the criminals of the world seem much more honorable. When scum rules the world, only more scum is born.",
+
+  "When do you think people die? When they are shot through the heart by the bullet of a pistol? No. When they are ravaged by an incurable disease? No... It’s when they're forgotten!",
+
+  "The cake is a lie",
+
+  "A man chooses! A slave obeys!",
+
+  "What do the numbers mean, Mason?",
+
+  "Tonight, Gehrman join the hunt",
+
+  "Farewell, good Hunter. May you find your worth in the waking world.",
+
+  "Curse here, Curse there. Curse for he and she, why care? A bottomless curse, a bottomless sea, source of all greatness, all things that be. Listen for the baneful chants. Weep with them, as one in trance. And weep with us, oh, weep with us.",
+
+  "Hunters have told me about the church. About the gods, and their love. But... do the gods love their creations? I am a doll, created by you humans. Would you ever think to love me? Of course... I do love you. Isn't that how you've made me?",
+
+  "Die! Die, die! Hunters are killers, nothing less! You call ME a beast? A Beast!? What would you know? I didn't ask for this!",
+
+  "Acts of goodness are not always wise, and acts of evil are not always foolish, but regardless, we shall always strive to be good.",
+
+  "Again and again, it never ends...Please, I need you to do something...this village is the true secret. Testament to the old sins. It feeds this Hunter's nightmare...Please, bring an end to the horror. So our forefathers sinned? We hunters cannot bear their weight forever...it isn't fair...it just isn't fair.",
+
+  "Do you know why the Hunters are drawn to this Nightmare? Because it sprouted from their very misdeeds. Things that some would rather keep secret. A pitiful tale of petty arrogance, really."
+ ]
 # Movie.all.each do |movie|
 #   (5...25).to_a.sample.times do
 #     Rating.create(amount: rating_amounts.sample , user: User.first, movie: movie)
@@ -73,6 +117,12 @@ def create_genres(genres)
     Genre.find_or_create_by(name: genre_name)
   end
 end
+
+# Movie.all.each do |movie|
+#   actors = movie.actors.uniq
+#   movie.actors.destroy_all
+#   movie.actors << actors
+# end
 
 # if Movie.all.count == 0
 #   initial_parse
