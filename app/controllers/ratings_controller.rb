c1f3f33dded5cf824703b047a4c966afdb21da8c
Derek Townsend <derekjavon2@gmail.com>
2019-08-14 15:15:45 -0500

    Ratings added

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,25 @@
 class RatingsController < ApplicationController
+  def create
+    rating = Rating.create(rating_params)
+    render json: MovieSerializer.new(rating.movie).to_serialized_json
+  end
+
+  def update
+    rating = Rating.find_by(id: params[:id])
+    rating.update_attributes(rating_params)
+    render json: MovieSerializer.new(rating.movie).to_serialized_json
+  end
+  
+  def destroy
+    rating = Rating.find_by(id: params[:id])
+    movie = rating.movie
+    rating.destroy
+    render json: MovieSerializer.new(movie).to_serialized_json
+  end
+
+
+  private
+  def rating_params
+    params.require(:rating).permit(:amount, :user_id, :movie_id)
+  end
 end
