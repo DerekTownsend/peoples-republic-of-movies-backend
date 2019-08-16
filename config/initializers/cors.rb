4bd762a6a72ed1c3450acb3b529770841abf16bf
Derek Townsend <derekjavon2@gmail.com>
2019-08-16 15:00:08 -0500

    Removing back slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://prmovies.herokuapp.com/'
+    origins 'https://prmovies.herokuapp.com'
 
     resource '*',
       headers: :any,
